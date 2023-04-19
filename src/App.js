39063f4686e1d2d30a718c9c0d2f4d28840985db
hmdalvin <63193506+hmdalvin@users.noreply.github.com>
2023-04-19 20:23:30 +0700

    first add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+// Function Component - Properties
+// function Biodata(props) {
+//   return <span>Umurnya {props.age} </span>
+// } 
+
+// function Greeting(props) {
+//   return <h1> Halo {props.name} - <Biodata age={props.age}/></h1>
+// }
+
+// State
+class Timer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: props.start
+    }
+  }
+
+  // Lifecycle
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval)
+  }
+
+  increase() {
+    //update state time every one second
+    this.setState((state, props) => ({
+      time: parseInt(state.time) + 1
+    }))
+  }
+
+
+  render() {
+    return (
+      <div> {this.state.time} </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Timer start="0"/>
+        {/* <Greeting name="Hilman" age="25"/> */}
+        {/* <Greeting name="Rasyi" age="15"/> */}
       </header>
     </div>
   );
